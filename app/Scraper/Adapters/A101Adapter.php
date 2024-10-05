4b5c76f39c36a66850543a7831f26cb8a4c70a96
yusuf <y.yilmaz@hotmail.com.tr>
2024-10-05 15:48:32 +0200

    composer update a101 fixed

--- app/Scraper/Adapters/A101Adapter.php
+++ app/Scraper/Adapters/A101Adapter.php
@@ -3,44 +3,52 @@
 namespace App\Scraper\Adapters;
 
 use App\Scraper\Adapter;
+use App\Scraper\Adapters\Contracts\HasRealPrice;
 use GuzzleHttp\Client;
-use GuzzleHttp\Cookie\CookieJar;
-use voku\helper\HtmlDomParser;
 
-class A101Adapter extends Adapter
+class A101Adapter extends Adapter implements HasRealPrice
 {
+    private \stdClass $priceContent;
+
     public function scrape()
     {
         $client = new Client();
-        $cookieJar = CookieJar::fromArray([
-            'countryCode' => 'TR',
-            'language' => 'tr',
-            'storefrontId' => 1,
-            'userid' => 'undefined'
-        ], 'a101.com.tr');
+        preg_match('/_p-(\d+)/', $this->url, $matches);
 
-        $request = $client->request(
+        $productId = $matches[1];
+
+        $priceContentUrl = 'https://rio.a101.com.tr/dbmk89vnr/CALL/Store/getProductBySku/C078?sku=' . $productId . '';
+        $priceRequest = $client->request(
             'GET',
-            $this->url,
-            ['cookies' => $cookieJar]
+            $priceContentUrl
         );
-        $this->html = HtmlDomParser::str_get_html($request->getBody()->getContents());
 
+        $priceContent = $priceRequest->getBody()->getContents();
+        $this->priceContent = json_decode($priceContent);
+
+        $productInfoUrl = 'https://rio.a101.com.tr/dbmk89vnr/CALL/Product/get/' . $productId . '';
+
+        $productInfoRequest = $client->request(
+            'GET',
+            $productInfoUrl
+        );
+        $productContent = $productInfoRequest->getBody()->getContents();
+        $this->json = json_decode($productContent);
     }
 
     public function getName(): string
     {
-        return $this->html->findOne('span.js-page-name-placeholder')->innerText();
+        return $this->json->attributes->name;
     }
 
     public function getImageUrl(): string
     {
-        return $this->html->findOne('img.js-product-image')->getAttribute('src');
+        return $this->json->images[0]->url;
     }
 
     public function getId(): string
     {
-        return $this->html->findOne('section.page-product')->getAttribute('data-pk');
+        return $this->json->id;
     }
 
     /**
@@ -48,12 +56,12 @@ class A101Adapter extends Adapter
      */
     public function getPrice()
     {
-        foreach ($this->html->find('meta') as $meta) {
-            if ($meta->getAttribute('property') === 'product:price:amount') {
-                return $meta->getAttribute('content');
-            }
-        }
+        return $this->priceContent->product->price->discountedStr;
+    }
 
+    public function getRealPrice(): string
+    {
+        return $this->priceContent->product->price->normalStr;
     }
 
     public function getCurrency(): string
